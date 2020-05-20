f913a25f15de0a0d27c0386768bac1b5ec35b952
David Lee <david.lee.sl2634@yale.edu>
2020-05-20 23:10:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,42 @@
 # require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file)
+   emoticons = YAML.load_file(file)
+   new_hash = {}
+   emoticons.each do |key,value|
+      if new_hash[key] == nil 
+         new_hash[key] = {}
+      end
+      if new_hash[key][:english] == nil 
+         new_hash[key][:english] = value[0]
+      end
+      if new_hash[key][:japanese] == nil 
+         new_hash[key][:japanese] = value[1]
+      end
+   end
+   new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_english_meaning(file, emoticon)
+  emoticons = load_library(file)
+    emoticons["get_meaning"].each do |key,value|
+      if emoticon == key 
+        return value
+      else
+        return "Sorry, that emoticon was not found."
+    end
+  end
 end
+      
 
-def get_english_meaning
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+  emoticons = load_lbrary(file)
+    emoticons["get_meaning"].each do |key, value|
+      if emoticon == japenese_emoticon
+        return meaning
+      else 
+        return "Sorry, that emoticon was not found."
+    end 
 end
