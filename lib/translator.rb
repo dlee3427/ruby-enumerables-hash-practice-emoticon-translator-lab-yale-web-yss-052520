20b3dd39a1a25bd4816bab044e7db8897b4bcc50
David Lee <david.lee.sl2634@yale.edu>
2020-05-20 23:20:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,31 +3,11 @@ require 'pry'
 require 'yaml'
 
 def load_library(file)
-   emoticons = YAML.load_file(file)
-   new_hash = {
-     "meaning" => {}
-     "emoticon" => {}
-     }
-    emoticons.each |
+   
 
 def get_english_meaning(file, emoticon)
-  emoticons = load_library(file)
-    emoticons["get_meaning"].each do |key,value|
-      if emoticon == key 
-        return value
-      else
-        return "Sorry, that emoticon was not found."
-    end
-  end
-end
+  
       
 
 def get_japanese_emoticon(file, emoticon)
-  emoticons = load_lbrary(file)
-    emoticons["get_meaning"].each do |key, value|
-      if emoticon == japenese_emoticon
-        return meaning
-      else 
-        return "Sorry, that emoticon was not found."
-    end 
-end
+  
