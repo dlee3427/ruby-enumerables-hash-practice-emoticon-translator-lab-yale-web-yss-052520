83850d2674237ba1e41a4c72577b2240e8735bdc
David Lee <david.lee.sl2634@yale.edu>
2020-05-20 23:20:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,21 @@ require 'pry'
 require 'yaml'
 
 def load_library(file)
-   
+   emoticons = YAML.load_file(file)
+   emoticon_translator = {
+      "definition" => {}
+      "emoticon" => {}
+   }
+   library.each do |key,value| 
+      english = value[0]
+      japanese = value[1]
+      emoticon_translator["definition"][japanese] = definition 
+      emoticon_translator["emoticon"][english] = japanese 
+   end
+   return emoticon_translator
+end
+
+
 
 def get_english_meaning(file, emoticon)
   
