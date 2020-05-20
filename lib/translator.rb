1c7e6db6daf9afae160186093a80b792b90b4383
David Lee <david.lee.sl2634@yale.edu>
2020-05-20 23:41:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_english_meaning(file, emoticon)
       if emoticons[translation][:japanese] == emoticon
         return translation
       end
-      if !emoticons[translation][:japanese] 
+      if emoticon != emoticons[translation][:japense]
         return "Sorry, that emoticon was not found"
       end
     end 
