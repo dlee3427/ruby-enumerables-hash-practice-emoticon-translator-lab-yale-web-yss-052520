d16a94483918136b0d4ec483920c5ad49b036c7f
David Lee <david.lee.sl2634@yale.edu>
2020-05-20 23:29:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,8 @@ def get_english_meaning(file, emoticon)
   emoticons = load_library(file)
   emoticons.each do |translation, languages|
     languages.each do |key, emoji|
-      if emoticon == translation[] 
+      if emoticon == translation[:japanese]
+        return 
     end 
   end
 end 
